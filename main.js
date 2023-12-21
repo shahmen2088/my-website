26d62169f0bfc5dbad2298240f0d4739a8085329
Shokhrukh <shoxrux20888@mail.ru>
2023-12-21 20:44:58 +0300

    AddMenu 50/100

--- main.js
+++ main.js
@@ -1,47 +1,14 @@
-// Получаем элементы слайдера
+// Бургер меню
 
-const sliders = document.querySelectorAll(".slider");
-const prevButton = document.querySelector(".prev-button");
-const nextButton = document.querySelector(".next-button");
+const menu = document.querySelector("#menu");
+const popup = document.querySelector("#popup");
 
-let slideCount = 0;
-let slideIndex = 0;
-// let currentSlider = 0;
+menu.addEventListener("click", menuHandler);
 
-function updateSlider() {
-  for (let i = 0; i < sliders.length; i++) {
-    const slides = sliders[i].querySelectorAll(".slider__photo");
-    console.log(slides.length);
-    slideCount = slides.length;
-    slides.forEach((slide, index) => {
-      if (index === slideIndex) {
-        slide.style.display = "block";
-      } else {
-        slide.style.display = "none";
-      }
-    });
-  }
+// Выполняем действие при клике
+function menuHandler(e) {
+  e.preventDefault();
+  // Переключаем стили при клике
+  menu.classList.toggle("open-menu");
+  popup.classList.toggle("open");
 }
-
-//Устанавливаем обработчик событий для кнопок
-
-prevButton.addEventListener("click", showPreviousSlide);
-nextButton.addEventListener("click", showNextSlide);
-
-//Функция для показа предыдущего слайда
-
-function showPreviousSlide() {
-  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-  updateSlider();
-}
-
-function showNextSlide() {
-  slideIndex = (slideIndex + 1) % slideCount;
-  updateSlider();
-}
-
-//Функция для обновления отображения слайдера
-
-//Инициализация слайдера
-
-updateSlider();
