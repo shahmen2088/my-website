3124e51d8dcbd1bc9d092e60dcee82d5ffa974c9
Shokhrukh <shoxrux20888@mail.ru>
2023-12-23 18:28:45 +0300

    AddScrollHeader

--- main.js
+++ main.js
@@ -2,13 +2,47 @@
 
 const menu = document.querySelector("#menu");
 const popup = document.querySelector("#popup");
+const header = document.querySelector("#header");
 
 menu.addEventListener("click", menuHandler);
 
-// Выполняем действие при клике
 function menuHandler(e) {
   e.preventDefault();
-  // Переключаем стили при клике
   menu.classList.toggle("open-menu");
-  popup.classList.toggle("open");
+  header.classList.toggle("open");
 }
+
+// Якорь smooth scroll
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach((item) => {
+  item.addEventListener("click", function (e) {
+    e.preventDefault();
+    const blockID = item.getAttribute("href");
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+//Скролл страницы
+
+var lastScrollTop = 0;
+
+function onScroll() {
+  window.addEventListener("scroll", callbackFunc);
+  function callbackFunc() {
+    var st = window.scrollY || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+      header.classList.remove("active");
+    } else if (st < lastScrollTop) {
+      header.classList.add("active");
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+  }
+}
+
+window.onload = function () {
+  onScroll();
+};
